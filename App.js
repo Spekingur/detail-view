1c6125c9702e43cec9ab0c541b8b29d8eeafd829
Maciuska <Maciuska14@gmail.com>
2018-10-31 19:29:05 +0000

    Added swap button

--- App.js
+++ App.js
@@ -1,56 +1,54 @@
-import React from 'react';
-import {
-  StyleSheet, Text, View, Image,
-} from 'react-native';
-import { Constants } from 'expo';
+import React from "react";
+import { StyleSheet, Button, Text, View, Image } from "react-native";
+import { Constants } from "expo";
 
 // const data = require('./data');
-import data from './data';
-import Homescreen from './src/components/HomeScreen';
-import Workscreen from './src/components/WorkScreen';
+import data from "./data";
+import Homescreen from "./src/components/HomeScreen";
+import Workscreen from "./src/components/WorkScreen";
 
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    backgroundColor: '#f09',
-    alignItems: 'center',
+    backgroundColor: "#f09",
+    alignItems: "center",
     // justifyContent: 'center',
-    marginTop: Constants.statusBarHeight,
+    marginTop: Constants.statusBarHeight
   },
   text: {
-    color: 'white',
+    color: "white"
   },
   avatarImg: {
     height: 200,
     width: 200,
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black"
   },
   imgStyle: {
     marginTop: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   nameSpace: {
     marginTop: 20,
-    flexDirection: 'row',
+    flexDirection: "row"
   },
   infoContainer: {
     marginLeft: 20,
-    marginRight: 20,
+    marginRight: 20
   },
   items: {
     borderBottomWidth: 1,
-    borderColor: 'black',
-  },
+    borderColor: "black"
+  }
 });
 
 const name = {
   firstname: data[0].name.first_name,
-  lastname: data[0].name.last_name,
+  lastname: data[0].name.last_name
 };
 
 const pic = {
-  uri: data[0].avatar,
+  uri: data[0].avatar
 };
 
 /*
@@ -73,21 +71,44 @@ const pic = {
 }; */
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.buttonText = "Show work info";
+    this.onPressButton = this.onPressButton.bind(this);
+    this.showingHomeInfoView = true;
+    this.switchAnimation = styles.infoContainer;
+  }
+
+  onPressButton() {
+    this.setState({
+      // buttonText: "Text has been changed"
+    });
+    if (this.showingHomeInfoView === true) {
+      this.buttonText = "Show home info";
+      this.showingHomeInfoView = false;
+    } else {
+      this.buttonText = "Show work info";
+      this.showingHomeInfoView = true;
+    }
+  }
+
   render() {
     return (
       <View style={styles.appContainer}>
         <View style={styles.imgStyle}>
           <Image style={styles.avatarImg} source={pic} />
         </View>
+
         <View style={styles.nameSpace}>
-          <Text style={styles.text}>
-            {name.firstname}
-            {' '}
-          </Text>
+          <Text style={styles.text}>{name.firstname} </Text>
           <Text style={styles.text}>{name.lastname}</Text>
         </View>
-        <Homescreen />
-        <Workscreen />
+        <View>
+          <Button title={this.buttonText} onPress={this.onPressButton} />
+        </View>
+        <View>
+          {this.showingHomeInfoView ? <Homescreen /> : <Workscreen />}
+        </View>
       </View>
     );
   }
